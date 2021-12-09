8342fc2ede73e157c2358d85afa6606a4c696383
Astromech-ai <79506266+Astromech-ai@users.noreply.github.com>
2021-12-09 14:44:16 +0100

    Timmer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeValue = 90;
-
+    public float timeValue = 30;
+    public Text timeText;
     void Update()
     {
         if (timeValue > 0)
@@ -16,14 +17,18 @@ public class Timer : MonoBehaviour
         {
             timeValue = 0;
         }
-        
+        DisplayTime(timeValue);
     }
     void DisplayTime(float timeToDisplay)
     {
-        if(timeToDisplay < 0)
+        if (timeToDisplay < 0)
         {
             timeToDisplay = 0;
         }
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+        
 }
