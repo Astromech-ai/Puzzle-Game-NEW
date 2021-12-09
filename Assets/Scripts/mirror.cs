6eed92f7f77d68f5c18f62925dfc3b2da07bd7e7
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2021-12-09 15:01:41 +0100

    Test

---
+++ Assets/Scripts/mirror.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirror : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
